82d79a0657e485a819917e550dc22085a16a24d4
MONDEM-DILEEP <24331a05i4@mvgrce.edu.in>
2025-12-26 14:40:05 +0530

    Add files via upload

---
+++ WEEK4-4_I4.py
@@ -0,0 +1,16 @@
+a=int(input("enter first number : "))
+b=int(input("enter second number : "))
+c=int(input("enter third number : "))
+if a>=b and a>=c : 
+        print(a," is the larger number  ")
+elif b>=a and b>=c : 
+        print(b," is the larger number  ")
+else : 
+        print(c," is the larger number ")
+
+if a<=b and a<=c : 
+        print(a," is the smaller number  ")
+elif b<=a and b<=c : 
+        print(b," is the smaller number  ")
+else : 
+        print(c," is the smaller number ")
